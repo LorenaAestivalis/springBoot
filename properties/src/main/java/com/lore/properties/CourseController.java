f1519cc056eb7cd3a9168566dcb291a078b6a1a6
lore <loreaestivalis@gmail.com>
2020-12-07 13:37:21 -0500

    this2

--- properties/src/main/java/com/lore/properties/CourseController.java
+++ properties/src/main/java/com/lore/properties/CourseController.java
@@ -1,8 +1,11 @@
 package com.lore.properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 
 @RestController
@@ -13,11 +16,27 @@ public class CourseController {
     @Value("${default.course.chapterCount}")
     private int chapterCount;
 
+    @Autowired
+    private CourseCOnfiguration courseConfiguration;
+
     @RequestMapping("/defautlCourse")
     Course getDefaultCourse(@RequestParam(value ="name",required =  false) String name,
                             @RequestParam(value = "chapterCount", defaultValue = "2", required = false)int chapterCount){
 
         return new Course(cName,chapterCount);
+    }
+    @RequestMapping("/defautlCourseP")
+    HashMap<String,Object> getConfigurationAnnotateProperties(){
+
+        HashMap<String,Object> map = new HashMap<String, Object>();
+
+        map.put("name",courseConfiguration.getName());
+        map.put("chapterCount",courseConfiguration.getChapterCount());
+        map.put("rating",courseConfiguration.getRating());
+        map.put("author",courseConfiguration.getAuthor());
+
+
+        return map;
     }
 
 
